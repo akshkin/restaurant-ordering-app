def0bd2cda3add4f617894e216766fca0de22108
akshkin <107682289+akshkin@users.noreply.github.com>
2023-01-11 11:57:08 +0100

    added rating functionality and meal-deal discount
    fixed isssues with total/changing quantity

---
+++ discount.js
@@ -0,0 +1,18 @@
+export default function discount(arr, total){
+    const discountEl = document.getElementById("discount") 
+    const food = arr.find(item => item.category === "food")
+    const drink = arr.find(item => item.category === "drink")
+    let discountAmount = 0
+
+    if (arr.includes(food) && arr.includes(drink)){
+        const discount = 15
+        discountAmount = (discount * total)/100
+        discountEl.style.paddingBlock = "1em"
+        discountEl.style.display = "flex"
+    } else{
+        discountEl.style.paddingBlock = "0"
+        discountEl.style.display = "none"
+    }
+    return discountAmount
+}
+
