484fe161cb4114a1ecddd4135912d499dbb0bc97
akshaya kini <akshaya.kini12@gmail.com>
2023-11-24 12:16:16 +0100

    Refactor code

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const menuArray = [
   {
-      name: "Pizza",
-      ingredients: ["pepperoni", "mushrom", "mozarella"],
-      id: 0,
-      price: 14,
-      emoji: "🍕",
-      category: "food"
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: "0",
+    price: 14,
+    emoji: "🍕",
+    category: "food",
   },
   {
-      name: "Hamburger",
-      ingredients: ["beef", "cheese", "lettuce"],
-      price: 12,
-      emoji: "🍔",
-      id: 1,
-      category: "food"
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: "1",
+    category: "food",
   },
-      {
-      name: "Beer",
-      ingredients: ["grain, hops, yeast, water"],
-      price: 12,
-      emoji: "🍺",
-      id: 2,
-      category: "drink"
-  }
-]
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "🍺",
+    id: "2",
+    category: "drink",
+  },
+];
 
-export default menuArray
+export default menuArray;
--- rating.js
+++ rating.js
@@ -1,55 +1,59 @@
-export default function rating(){
-    let rating = 0
-    const stars = []
-    stars.length = 5
-    stars.fill(`<img src="/images/star-outline.svg" alt="star"/>`)
-    const starDiv = document.getElementById("stars")
-    starDiv.innerHTML = `
-        ${stars.map((star, index) => `<span data-rating="${index + 1}" class="outlined-star">${star}</span>`).join("")}
-    `
-    
-    function renderChanges(rating){
-        for (let i = 0; i < rating; i++){
-            starDiv.children[i].classList.add("star-filled")
-        }
-        //non-highlight stars after the rating
-        for (let i = rating; i < 5; i++){
-            starDiv.children[i].classList.remove("star-filled")
-        }
-      }
+export default function rating() {
+  let rating = 0;
+  const stars = [];
+  stars.length = 5;
+  stars.fill(`<img src="/images/star-outline.svg" alt="star"/>`);
+  const starDiv = document.getElementById("stars");
+  starDiv.innerHTML = `
+        ${stars
+          .map(
+            (star, index) =>
+              `<span data-rating="${
+                index + 1
+              }" class="outlined-star">${star}</span>`
+          )
+          .join("")}
+    `;
 
-      function changeRating(newRating){
-        rating = newRating
-      }
-    
-      function getRating(){
-        return rating           
-      }
-    
-      function onMouseOver(event){
-        let star = event.target.parentElement
-        let isStar = star.classList.contains("outlined-star")
-        if(isStar){
-            let rating = event.target.parentElement.dataset.rating
-            renderChanges(rating) 
-        }
-      }
-    
-    function onMouseClick(event){
-        let star = event.target.parentElement
-        console.log(star)
-        let isStar = star.classList.contains("outlined-star")
-        if(isStar){
-            let rating = event.target.parentElement.dataset.rating
-            rating = rating === getRating() ? 0 : rating
-            renderChanges(rating) 
-            changeRating(rating)
-        }
+  function renderChanges(rating) {
+    for (let i = 0; i < rating; i++) {
+      starDiv.children[i].classList.add("star-filled");
     }
+    //non-highlight stars after the rating
+    for (let i = rating; i < 5; i++) {
+      starDiv.children[i].classList.remove("star-filled");
+    }
+  }
 
-    starDiv.addEventListener("mouseover", onMouseOver)
-    starDiv.addEventListener("click", onMouseClick)
-      
-    
+  function changeRating(newRating) {
+    rating = newRating;
+  }
+
+  function getRating() {
+    return rating;
+  }
+
+  function onMouseOver(event) {
+    let star = event.target.parentElement;
+    let isStar = star.classList.contains("outlined-star");
+    if (isStar) {
+      let rating = event.target.parentElement.dataset.rating;
+      renderChanges(rating);
+    }
+  }
+
+  function onMouseClick(event) {
+    let star = event.target.parentElement;
+
+    let isStar = star.classList.contains("outlined-star");
+    if (isStar) {
+      let rating = event.target.parentElement.dataset.rating;
+      rating = rating === getRating() ? 0 : rating;
+      renderChanges(rating);
+      changeRating(rating);
+    }
+  }
+
+  starDiv.addEventListener("mouseover", onMouseOver);
+  starDiv.addEventListener("click", onMouseClick);
 }
-
